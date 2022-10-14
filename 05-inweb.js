9434402c7a5b56dfa1d61ec9a37309590027a7a5
luigicallero <luigicallero@gmail.com>
2022-10-14 02:10:08 -0300

    learning more of JS

---
+++ 05-inweb.js
@@ -0,0 +1,12 @@
+document.write("Welcome to My world!!")
+
+console.log("We are experimenting with JS woooow")
+
+// Declaring variables
+let myVariable
+
+// Instantiating variables
+myVariable = 'Luisca is here!!'
+
+// Showing the variable value in Screen
+document.write(myVariable)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JavaScript-Basics
 Practicing JS basics
+
+Following now this very good tutorial in spanish:
+https://www.youtube.com/watch?v=gRXujAIFsLo
+
+Continue from minute 28 ;)
