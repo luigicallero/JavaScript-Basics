bf074c01b5d3409ea81fd655c2045d7747f8eecd
luigicallero <luigicallero@gmail.com>
2023-03-11 21:07:43 -0300

    working with js

--- 05-blockdemy1.js
+++ 05-blockdemy1.js
@@ -3,7 +3,7 @@ document.write("Welcome to My world!! ")
 console.log("We are experimenting with JS woooow")
 
 // Declaring variables
-var oldVariable // "var" can be used by old version for "let"
+var oldVariable // "var" can be used by old version instead of "let"
 let myVariable
 
 // Instantiating variables
@@ -21,3 +21,16 @@ let yourName = prompt("Please insert your Name: ")
 
 console.log(yourName)
 
+// Numbers
+let number1 = parseInt(prompt("Insert a number: "))
+//number1= parseInt(number1)
+
+let number2 = prompt("Insert a number: ")
+number2 =  parseInt(number2)
+
+let result
+result = number1 + number2
+
+console.log("Your result is:", result)
+
+document.getElementById("texto").innerHTML = "Content has changed"
