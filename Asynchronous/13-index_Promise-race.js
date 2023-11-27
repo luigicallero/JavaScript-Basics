8e7679a64b8c1fc192a899709b554a3500ffe1bf
luis-callero <luis.callero@kyndryl.com>
2023-11-27 17:01:16 +1300

    more promise object options

--- Asynchronous/13-index_Promise-race.js
+++ Asynchronous/13-index_Promise-race.js
@@ -1,19 +1,13 @@
-// const pf1 = new Promise((resolve, reject)=>{
-//     resolve("here is the data");
-// })
-
 const pf1 = Promise.resolve("pf1 done!!");
-//pf1.then(data => console.log(data));
 //const pf2 = Promise.resolve("pf2 done!!");
-const pf2 = Promise.reject("pf2 done!!");
-//pf2.then(data => console.log(data));
+const pf2 = Promise.reject("Error in the system!!");
 const pf3 = new Promise((resolve, reject) => {
     setTimeout(resolve, 2000, "pf3 done!!"); // resolve function to be executed at 0 returning 2022
 });
 
-// another method "all" from Promise
+// another method "any" from Promise
 Promise
-.all([pf1, pf2, pf3])
+.race([pf2, pf1, pf3 ]) // first one to resolve or fail is the end of function
 .then(data => {
     console.log(data);
 })
